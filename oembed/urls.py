2b950bddb8f7de94078a078b7b5d359bd53a75f0
Mike Hurt <mhurt@linux-9b07.site>
2014-07-17 07:02:38 +0100

    Updated url imports

--- oembed/urls.py
+++ oembed/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import url
+from oembed import views
 
-urlpatterns = patterns('oembed.views',
-    url(r'^$', 'oembed_schema', name='oembed_schema'),
-    url(r'^json/$', 'json', name='oembed_json'),
-    url(r'^consume/json/$', 'consume_json', name='oembed_consume_json'),
-)
+urlpatterns = [
+    url(r'^$', views.oembed_schema, name='oembed_schema'),
+    url(r'^json/$', views.json, name='oembed_json'),
+    url(r'^consume/json/$', views.consume_json, name='oembed_consume_json'),
+    ]
